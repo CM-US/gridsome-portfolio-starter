5162372171feaa56220823f1c14aadc2fc67127d
Ckarst <cody@cadencemedia.us>
2021-01-28 21:58:47 -0700

    updated site url

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,7 +7,7 @@
 module.exports = {
   siteName: 'Gridsome Portfolio Starter',
   siteDescription: 'A simple portfolio theme for Gridsome powered by Tailwind CSS v1',
-  siteUrl: 'https://gridsome-portfolio-starter.netlify.com',
+  siteUrl: 'https://njordworks-test.netlify.app',
   plugins: [
     {
       use: 'gridsome-plugin-tailwindcss',
@@ -43,13 +43,13 @@ module.exports = {
         contentTypeName: 'Post',
         feedOptions: {
           title: 'Gridsome Portfolio Starter Blog',
-          feed_url: 'https://gridsome-portfolio-starter.netlify.com/rss.xml',
-          site_url: 'https://gridsome-portfolio-starter.netlify.com/'
+          feed_url: 'https://njordworks-test.netlify.app/rss.xml',
+          site_url: 'https://njordworks-test.netlify.app/'
         },
         feedItemOptions: node => ({
           title: node.title,
           description: node.summary,
-          url: 'https://gridsome-portfolio-starter.netlify.com' + node.path,
+          url: 'https://njordworks-test.netlify.app' + node.path,
           author: 'Andre Madarang',
           date: node.date
         }),
